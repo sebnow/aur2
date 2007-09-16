83e61994d3776e061a20998f2cb11d48e38e8f12
Sebastian Nowicki <sebnow@gmail.com>
2007-09-16 20:30:32 +0800

    Added package_detail generic view.
    Added a link from package_archive to package_detail
    Added new url entry for it
    Removed pointless "/?" from urls

--- archlinux/aur/urls.py
+++ archlinux/aur/urls.py
@@ -7,5 +7,7 @@ info_dict = {
 }
 
 urlpatterns = patterns('django.views.generic',
-    (r'^/?$', 'date_based.archive_index', info_dict),
+    (r'^package/(?P<object_id>[a-zA-Z0-9_]+)/$', 'list_detail.object_detail',
+        dict(queryset=Package.objects.all(), template_object_name='pkg')),
+    (r'^$', 'date_based.archive_index', info_dict),
 )
