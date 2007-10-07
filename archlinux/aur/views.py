89602bd61ac38b2b38226dd1ce5d132580ad8911
Sebastian Nowicki <sebnow@gmail.com>
2007-10-08 00:35:33 +0800

    Improved searching
    * Made query optional, as to allow a listing of all packages
    * Removed useless things from sort by
    * Changed some things like location for consitency
    * Searching by maintainer is now possible
    * Fixed restricting repositories
    * Can search for packages updated since lastupdate
    * Added sorting and sort order

--- archlinux/aur/views.py
+++ archlinux/aur/views.py
@@ -4,25 +4,52 @@ from django import newforms as forms # This will change to forms in 0.68 or 1.0
 from archlinux.aur.models import Package, PackageSearchForm
 
 def search(request, query = ''):
-    context = dict()
-    if request.method == 'GET':
-
+    if request.method == 'GET' and request.GET.has_key('query'):
         form = PackageSearchForm(request.GET)
         # If there are any errors in the forum, render the template and exit
         if not form.is_valid():
             return render_to_response('aur/search.html', {'form': form})
-
-        res1 = Package.objects.filter(name__icontains=form.cleaned_data["query"])
+        
+        # Find the packages by searching description and package name or maintainer
+        if form.cleaned_data["searchby"] == 'maintainer':
+            res1 = Package.objects.filter(maintainers__username__icontains=form.cleaned_data["query"])
+        else:
+            res1 = Package.objects.filter(name__icontains=form.cleaned_data["query"])
         res2 = Package.objects.filter(description__icontains=form.cleaned_data["query"])
         results = res1 | res2
 
     else:
-        form = PackageSearchForm()
+        form_data = {
+            'repository': 'all',
+            'category': 'all',
+            'searchby': 'name',
+            'sortby': 'name',
+            'order': 'asc',
+            'limit': '25',
+        }
+        form = PackageSearchForm(form_data)
+
+        # We need to validate to get form.cleaned_data
+        if not form.is_valid():
+            return render_to_response('aur/search.html', {'form': form})
+
         results = Package.objects.all()
 
+    # Restrict results
     if form.cleaned_data["repository"] != 'all':
-        results = results.filter(repository__name__exact=form.cleaned_data["repository"])
+        results = results.filter(repository__name__iexact=form.cleaned_data["repository"])
     if form.cleaned_data["category"] != 'all':
         results = results.filter(category__name__exact=form.cleaned_data["category"])
+    if form.cleaned_data["lastupdate"]:
+        results = results.filter(updated__gte=form.cleaned_data["lastupdate"])
+
+    # Sort
+    sortby = form.cleaned_data["sortby"]
+
+    # Change the sort order if necessary
+    if form.cleaned_data["order"] == 'desc':
+        results = results.order_by('-' + sortby, 'repository', 'category', 'name')
+    else:
+        results = results.order_by(sortby, 'repository', 'category', 'name')
 
     return render_to_response('aur/search.html', {'form': form, 'packages': results})
