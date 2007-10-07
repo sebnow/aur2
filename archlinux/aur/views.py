632b860f7ee5e993ba564a4f4399af3a3a4dba21
Sebastian Nowicki <sebnow@gmail.com>
2007-10-07 19:48:16 +0800

    Added search template and removed some crap from views.py
    Removed various comments as the uncommented stuff works well.
    The view currently displays the search form well but doesn't do
    anything beyond that.

--- archlinux/aur/views.py
+++ archlinux/aur/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from django import newforms as forms # This will change to forms in 0.68 or 1.0
 
-from archlinux.aur.models import Package PackageSearchForm
+from archlinux.aur.models import Package, PackageSearchForm
 
 def search(request, query = ''):
     context = dict()
@@ -11,18 +10,6 @@ def search(request, query = ''):
             # take the q GET var over the one passed on the URL
             query = request.GET['query'].strip()
 
-#    form_data = {
-#            'query':        query,
-#            'repository':   request.GET.get('repo', 'all'),
-#            'category':     request.GET.get('category', 'all'),
-#            'lastupdate':   request.GET.get('lastupdate', ''),
-#            'limit':        int(request.GET.get('limit', '50')),
-#            'sortby':       request.GET.get('sortby', ''),
-#            'searchby':     request.GET.get('sortorder', ''),
-#            'searchby':     request.GET.get('searchby', 'name'),
-#    }
-
-#    form = PackageSearchForm(form_data)
         form = PackageSearchForm(request.GET)
         # If there are any errors in the forum, render the template and exit
         if not form.is_valid():
@@ -31,5 +18,4 @@ def search(request, query = ''):
     else:
         form = PackageSearchForm()
 
-    context['form'] = form;
-    return render_to_response('aur/search.html', Context(Context))
+    return render_to_response('aur/search.html', {'form': form})
