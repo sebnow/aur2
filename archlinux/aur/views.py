bb5f967e631c079bb3fc18c8cac84d0ddf73e440
Sebastian Nowicki <sebnow@gmail.com>
2007-10-07 21:57:57 +0800

    Searching now actually works.
    Only category and repository restricts, the other fields are not used
    yet.

--- archlinux/aur/views.py
+++ archlinux/aur/views.py
@@ -6,16 +6,23 @@ from archlinux.aur.models import Package, PackageSearchForm
 def search(request, query = ''):
     context = dict()
     if request.method == 'GET':
-        if request.GET.has_key('query'):
-            # take the q GET var over the one passed on the URL
-            query = request.GET['query'].strip()
 
         form = PackageSearchForm(request.GET)
         # If there are any errors in the forum, render the template and exit
         if not form.is_valid():
             return render_to_response('aur/search.html', {'form': form})
 
+        res1 = Package.objects.filter(name__icontains=form.cleaned_data["query"])
+        res2 = Package.objects.filter(description__icontains=form.cleaned_data["query"])
+        results = res1 | res2
+
     else:
         form = PackageSearchForm()
+        results = Package.objects.all()
 
-    return render_to_response('aur/search.html', {'form': form})
+    if form.cleaned_data["repository"] != 'all':
+        results = results.filter(repository__name__exact=form.cleaned_data["repository"])
+    if form.cleaned_data["category"] != 'all':
+        results = results.filter(category__name__exact=form.cleaned_data["category"])
+
+    return render_to_response('aur/search.html', {'form': form, 'packages': results})
