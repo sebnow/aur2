b2c7ccfeda8d3a0313119fc6af2a2ca9a1ac593e
Sebastian Nowicki <sebnow@gmail.com>
2007-10-08 18:23:55 +0800

    Added login and logged_out views

--- archlinux/urls.py
+++ archlinux/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
 
     # Uncomment this for admin:
     (r'^admin/', include('django.contrib.admin.urls')),
+    url(r'^accounts/logout/', 'django.contrib.auth.views.logout', name='user_logout'),
+    url(r'^accounts/login/', 'django.contrib.auth.views.login', name='user_login'),
     (r'^', include('archlinux.aur.urls')),
 )
