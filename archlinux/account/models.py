c702a1906951e769c876cde424468f1e6c845eeb
Sebastian Nowicki <sebnow@gmail.com>
2007-10-09 17:01:01 +0800

    Users can now modify their account details

--- archlinux/account/models.py
+++ archlinux/account/models.py
@@ -8,3 +8,10 @@ class RegistrationForm(forms.Form):
     last_name = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=75)
     password = forms.CharField(max_length=30, widget=forms.PasswordInput)
+
+class ProfileUpdateForm(forms.Form):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=75, required=False)
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput, required=False, initial='')
+    password_repeat = forms.CharField(max_length=30, widget=forms.PasswordInput, required=False, initial='')
