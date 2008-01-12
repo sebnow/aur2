50d2d153b4b24fd8f9171a5d2f83fe97837f09df
Sebastian Nowicki <xilonmu@gmail.com>
2008-01-12 22:55:02 +0900

    Refactored submit code into PackageSubmitForm
    Validation and actual saving of data and files is now separated. Errors are
    enclosed within the form

--- archlinux/aur/views.py
+++ archlinux/aur/views.py
@@ -10,11 +10,8 @@ from BetterPaginator import BetterPaginator
 from django import newforms as forms # This will change to forms in 0.68 or 1.0
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.db import transaction
-from django.core import serializers
 
 from archlinux.aur.models import *
-import archlinux.aur.Package as PKGBUILD
 
 def search(request, query = ''):
     if request.method == 'GET' and request.GET.has_key('query'):
@@ -51,165 +48,15 @@ def search(request, query = ''):
 
 # TODO: Implement transactions
 @login_required
-@transaction.commit_manually
 def submit(request):
     if request.method == 'POST':
         form = PackageSubmitForm(request.POST, request.FILES)
-        if not form.is_valid():
-            return render_to_response('aur/submit.html', {
-                'user': request.user, 'form': form})
-        import tempfile
-        directory = tempfile.mkdtemp()
-        filename = os.path.join(directory, form.cleaned_data['file'].filename)
-        # Save the uploaded file to disk
-        fp = open(filename, "wb")
-        fp.write(form.cleaned_data['file'].content)
-        fp.close()
-
-        try:
-            pkg = PKGBUILD.Package(filename)
-        except:
-            # TODO: Add error to form
-            return render_to_response('aur/submit.html', {
-                'user': request.user, 'form': form, 'errors': sys.exc_info()[1]})
-        pkg.validate()
-        if not pkg.is_valid() or pkg.has_warnings():
-            return render_to_response('aur/submit.html', {
-                'user': request.user, 'form': form, 'errors': pkg.get_errors(),
-                'warnings': pkg.get_warnings()})
-
-        # TODO: Check if we are updating an existing package or creating one
-
-        package = Package(name=pkg['name'], version=pkg['version'],
-                release=pkg['release'], description=pkg['description'],
-                url=pkg['url'])
-        package.repository=Repository.objects.get(name__exact="Unsupported")
-        package.category=Category.objects.get(name__iexact=form.cleaned_data['category'])
-        # Save the package so we can reference it
-        package.save()
-
-        # Check for, and add dependencies
-        for dependency in pkg['depends']:
-            # This would be nice, but we don't have access to the official
-            # repositories
-            #if Package.objects.filter(name=dependency).count() == 0:
-                # TODO: Display an error
-                #return render_to_response("aur/submit.html")
-            try:
-                dep = Package.objects.get(name__exact=dependency)
-            except Package.DoesNotExist:
-                # Fail silently
-                pass
-            else:
-                package.depends.add(dep)
-
-        package.maintainers.add(request.user)
-        for license in pkg['licenses']:
-            object, created = License.objects.get_or_create(name=license)
-            package.licenses.add(object)
-        for arch in pkg['arch']:
-            try:
-                object = Architecture.objects.get(name=arch)
-            except Architecture.DoesNotExist:
-                # TODO: Add an error
-                transaction.rollback()
-                return render_to_response('aur/submit.html', {
-                    'user': request.user,
-                    'form': form,
-                    'errors': ['architecture %s does not exist' % arch,],
-                })
-            else:
-                package.architectures.add(object)
-
-        # Check if the uploaded file is a tar file or just a PKGBUILD
-        try:
-            tar = tarfile.open(filename, "r")
-        except tarfile.ReadError:
-            # It's not a tar file, so if must be a PKGBUILD since it validated
-            is_tarfile = False
-            pkgbuild = filename
-            pass
-        else:
-            is_tarfile = True
-            tmpdir_sources = os.path.join(directory, 'sources')
-            tar.extractall(tmpdir_sources)
-            pkgbuild = os.path.join(tmpdir_sources, package.name, 'PKGBUILD')
-
-        # Hash and save PKGBUILD
-        fp = open(pkgbuild, "r")
-        pkgbuild_contents = ''.join(fp.readlines())
-        fp.close()
-        source = PackageFile(package=package)
-        source.save_filename_file('%s/sources/PKGBUILD' % package.name,
-                pkgbuild_contents)
-        source.save()
-        md5hash = hashlib.md5(pkgbuild_contents)
-        hash = PackageHash(hash=md5hash.hexdigest(), file=source, type='md5')
-        hash.save()
-        del pkgbuild_contents
-
-        # Save tarball
-        # TODO: Tar the saved sources instead of using the uploaded one, for
-        # security
-        if not is_tarfile:
-            # We only have to PKGBUILD, so lets make a tarball
-            tar = tarfile.open(os.path.join(directory,
-                '%s.tar.gz' % package.name), "w|gz")
-            tar.add(filename, '%s/PKGBUILD' % package.name)
-            tar.close()
-            filename = os.path.join(directory, '%s.tar.gz' % package.name)
-        fp = open(filename, "rb")
-        package.save_tarball_file('%s/%s' % (package.name,
-            os.path.basename(filename)), ''.join(fp.readlines()))
-        fp.close()
-
-        for index in range(len(pkg['source'])):
-            source_filename = pkg['source'][index]
-            source = PackageFile(package=package)
-            # If it's a local file, save to disk, otherwise record as url
-            if is_tarfile and os.path.exists(os.path.join(tmpdir_sources,
-               package.name, source_filename)):
-                    fp = open(os.path.join(tmpdir_sources, package.name,
-                        source_filename), "r")
-                    source.save_filename_file('%s/sources/%s' % (package.name,
-                        source_filename), ''.join(fp.readlines()))
-                    fp.close()
-            else:
-                # TODO: Check that it _is_ a url, otherwise report an error
-                # that files are missing
-                source.url = source_filename
-            source.save()
-            # Check for, and save, any hashes this file may have
-            for hash_type in ('md5', 'sha1', 'sha256', 'sha384', 'sha512'):
-                if pkg[hash_type + 'sums']:
-                    PackageHash(hash=pkg[hash_type + 'sums'][index],
-                            file=source, type=hash_type).save()
-
-        for install_file in pkg['install']:
-            source_path = os.path.join(tmpdir_sources, package.name,
-                    install_file)
-            if not os.path.exists(source_path):
-                self._errors.append()
-                transaction.rollback()
-                return render_to_response('aur/submit.html', {
-                    'user': request.user,
-                    'form': form,
-                    'errors': 'install file "%s" does not exist' % install_file
-                })
-
-            source = PackageFile(package=package)
-            fp = open(source_path, "r")
-            source.save_filename_file('%s/install/%s' % (package.name,
-                    install_file), ''.join(fp.readlines()))
-            fp.close()
-            source.save()
-
-        transaction.commit()
-        return HttpResponseRedirect(
-                reverse('aur-package_detail', args=[package.name,]))
+        if form.is_valid():
+            form.save(request.user)
+            return HttpResponseRedirect(reverse('aur-package_detail',
+                args=[form.cleaned_data['package']['name'],]))
     else:
         form = PackageSubmitForm()
-
     return render_to_response('aur/submit.html', {
         'user': request.user,
         'form': form,
