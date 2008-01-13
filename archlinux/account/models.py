0498cc79ab009c580c6eb5f7a097234a3807e476
Sebastian Nowicki <sebnow@gmail.com>
2008-01-13 22:21:35 +0900

    Extended User model

---
+++ archlinux/account/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.encoding import smart_unicode
+from archlinux.aur.models import Package
+
+class ArchUser(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    is_inactive = models.BooleanField(default=False)
+    irc_nick = models.CharField(max_length=16)
+
+    def get_packages(self):
+        return Package.objects.filter(maintainers__username=self.user.username)
+
+    def can_modify_package(self, package):
+        if not isinstance(package, Package):
+            package = Package.objects.get(name=package)
+        return self.is_maintainer(package) or self.is_moderator()
+
+    def can_delete_package(self, package):
+        return self.is_moderator()
+
+    def is_maintainer(self, package):
+        if not isinstance(package, Package):
+            package = Package.objects.get(name=package)
+        return package.maintainers.filter(username=self.user.username).count() > 0
+
+    def is_moderator(self):
+        is_trusted_user = self.user.groups.filter(name="Trusted User").count() > 0
+        is_developer = self.user.groups.filter(name="Developer").count() > 0
+        return is_trusted_user or is_developer
+
+    def __unicode__(self):
+        return smart_unicode(self.user)
+
+    class Admin:
+        list_display = ('user', 'is_inactive')
--- archlinux/account/views.py
+++ archlinux/account/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.db import transaction
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
@@ -7,6 +8,7 @@ from django.http import HttpResponseRedirect
 from archlinux.account.forms import RegistrationForm, ProfileUpdateForm
 from archlinux.aur.models import Package
 
+@transaction.commit_manually
 def register(request):
     # Are we submitting or just displaying?
     if request.method == 'POST':
@@ -23,13 +25,23 @@ def register(request):
         )
         user.first_name = form.cleaned_data["first_name"]
         user.last_name = form.cleaned_data["last_name"]
+        user.groups.add(Group.objects.get(name='User'))
         user.save()
+        ArchUser(
+                user = user,
+                irc_nick = form.cleaned_data['irc_nick']
+        ).save()
+        transaction.commit()
         return HttpResponseRedirect(reverse(profile))
 
     form = RegistrationForm()
-    return render_to_response('registration/register.html', {'form': form, 'user': request.user})
+    return render_to_response('registration/register.html', {
+        'form': form,
+        'user': request.user
+    })
 
 @login_required
+@transaction.commit_manually
 def update_profile(request):
     if request.method != 'POST':
         return HttpResponseRedirect(reverse(profile))
@@ -47,17 +59,23 @@ def update_profile(request):
         request.user.email = form.cleaned_data["email"]
     if form.cleaned_data["password"] and form.cleaned_data["password"] == form.cleaned_data["password_repeat"]:
         request.user.set_password(form.cleaned_data["password"])
+    profile = request.user.get_profile()
+    if form.cleaned_data["irc_nick"] and form.cleaned_data["irc_nick"] != profile.irc_nick:
+        profile.irc_nick = form.cleaned_data['irc_nick']
+        profile.save()
 
     request.user.save()
-    return HttpResponseRedirect(reverse(profile))
+    transaction.commit()
+    return HttpResponseRedirect(reverse('account-my_profile'))
 
 @login_required
 def profile(request):
-    packages = Package.objects.filter(maintainers__username__exact=request.user.username)
+    packages = Package.objects.filter(maintainers__username=request.user.username)
     form = ProfileUpdateForm({
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
         'email': request.user.email,
+        'irc_nick': request.user.get_profile().irc_nick,
     })
 
     return render_to_response('registration/profile.html', {
