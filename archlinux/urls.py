680f25ec8e5c1c56f5470ed919c2d5ee6d60ef2c
Sebastian Nowicki <sebnow@gmail.com>
2009-02-10 12:29:39 +0900

    Fix serving static media using the Django dev server
    Signed-off-by: Sebastian Nowicki <sebnow@gmail.com>

--- archlinux/urls.py
+++ archlinux/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^archlinux/', include('archlinux.foo.urls')),
-
-    # Uncomment this for admin:
     (r'^admin/(.*)', admin.site.root),
     (r'^accounts/', include('registration.urls')),
     (r'^profile/', include('aurprofile.urls')),
     (r'^', include('archlinux.aur.urls')),
 )
+
+if settings.DEBUG == True:
+	urlpatterns += patterns('',
+		(r'^media/(.*)$', 'django.views.static.serve', {
+		    'document_root': settings.MEDIA_ROOT
+		})
+	)
